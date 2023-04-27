f80b70298415d8ae8c98826b7be6578d84a78326
Bruno <bruno.pacheco@tusta.com.br>
2023-04-27 00:29:36 -0300

    feat: refactor credithub controller

--- src/Api/Controllers/CreditHubController.cs
+++ src/Api/Controllers/CreditHubController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Project.Module.CreditHub.Application.UseCases.GetNewSomething;
 
 namespace Api.Controllers
 {
@@ -8,9 +10,21 @@ namespace Api.Controllers
     {
         [HttpGet]
         [Route("test")]
-        public async Task<IActionResult> test()
+        public async Task<IActionResult> test(
+            [FromBody] Dto dto,
+            [FromServices] IMediator mediator
+        )
         {
-            return Ok();
+            var request = new GetNewSomethingRequest(dto.Number);
+
+            var responde =  await mediator.Send(request);
+
+            return Ok(responde);
         }
+    }
+
+    public class Dto
+    {
+        public int Number { get; set; }
     }
 }
