88291947d7935342dc77d05d20efbccfdeaa31cd
Bruno <bruno.pacheco@tusta.com.br>
2023-04-27 16:12:29 -0300

    feat: refactor specification

--- src/Module/CreditHub/CreditHub.Domain/Specification/CanReleaseLoanCredit.cs
+++ src/Module/CreditHub/CreditHub.Domain/Specification/CanReleaseLoanCredit.cs
@@ -22,42 +22,21 @@ namespace Project.Module.CreditHub.Domain.Specification
             FirstDueDate = firstDueDate;
         }
 
-        public bool IsSatisfied()
+        public bool IsSatisfied(string loanTypeName)
         {
-            var loan = CreateLoanBasedOnLoanCreditType(LoanCreditType);
-
             if(!(Value <= 1000000.00))
                 return false;
 
-            if(NumberOfInstallments >= 5 && NumberOfInstallments <= 75)
+            if(!(NumberOfInstallments >= 5) || !(NumberOfInstallments <= 75))
                 return false;
 
-            if(!(FirstDueDate >= DateOnly.FromDateTime(DateTime.UtcNow.AddDays(15))) && (FirstDueDate >= DateOnly.FromDateTime(DateTime.UtcNow.AddDays(40))))
+            if(!(FirstDueDate >= DateOnly.FromDateTime(DateTime.UtcNow.AddDays(15))) || !(FirstDueDate <= DateOnly.FromDateTime(DateTime.UtcNow.AddDays(40))))
                 return false; 
             
-            if(loan.GetName() == "LegalLoan")
+            if(loanTypeName == "LegalLoan")
                 return Value >= 15000.00;
 
             return true;
-        }
-
-        private Loan CreateLoanBasedOnLoanCreditType(string loanCreditType)
-        {
-            switch (loanCreditType)
-            {
-                case "Crédito Direto":
-                    return new PaydayLoan();
-                case "Crédito Consignado":
-                    return  new PaydayLoan();
-                case "Crédito Pessoa Jurídica":
-                    return  new LegalLoan();
-                case "Crédito Pessoa Física":
-                    return  new NaturalPersonLoan();
-                case "Crédito Imobiliário":
-                    return  new HomeEquityLoan();
-            }
-
-            throw new Exception("Não existe implementação para o crédito solicitado a seguir: ${loanCreditType}");
         }
     }
 }
