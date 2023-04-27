70c368871cd17079f74e5e53e1a7e419a7c42600
Bruno <bruno.pacheco@tusta.com.br>
2023-04-27 10:52:44 -0300

    test: create test with all use cases

---
+++ src/Module/CreditHub/CreditHub.UnitTest/Application/UseCases/RequestLoanCredit/RequestLoanCreditHandlerTest.cs
@@ -0,0 +1,46 @@
+namespace Credithub.UnitTest;
+
+public class RequestLoanCreditHandlerTest
+{
+    [Fact]
+    public void Request_loan_credit_is_reproved_when_value_is_over_of_specified()
+    {
+        
+    }
+
+    [Fact]
+    public void Request_loan_credit_is_reproved_when_installment_is_less_of_specified()
+    {
+        
+    }
+
+    [Fact]
+    public void Request_loan_credit_is_reproved_when_installment_is_over_of_specified()
+    {
+        
+    }
+
+    [Fact]
+    public void Request_loan_credit_is_reproved_when_value_is_less_of_specified_for_pj()
+    {
+        
+    }
+
+    [Fact]
+    public void Request_loan_credit_is_reproved_when_first_due_date_is_less_of_specified()
+    {
+        
+    }
+
+    [Fact]
+    public void Request_loan_credit_is_reproved_when_first_due_date_is_over_of_specified()
+    {
+
+    }
+
+    [Fact]
+    public void Request_loan_credit_is_approved_when_filled_the_specifications()
+    {
+
+    }
+}             
