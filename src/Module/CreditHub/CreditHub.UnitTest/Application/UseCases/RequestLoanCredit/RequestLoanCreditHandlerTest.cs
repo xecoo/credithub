aba494cbda55829f0028733b4d3414956b795fd3
Bruno <bruno.pacheco@tusta.com.br>
2023-04-27 16:14:26 -0300

    test: test handle with many use cases based on specification

--- src/Module/CreditHub/CreditHub.UnitTest/Application/UseCases/RequestLoanCredit/RequestLoanCreditHandlerTest.cs
+++ src/Module/CreditHub/CreditHub.UnitTest/Application/UseCases/RequestLoanCredit/RequestLoanCreditHandlerTest.cs
@@ -6,39 +6,99 @@ namespace Credithub.UnitTest;
 public class RequestLoanCreditHandlerTest
 {
     [Fact]
-    public void Request_loan_credit_is_reproved_when_value_is_over_of_specified()
+    public async Task Request_loan_credit_is_reproved_when_value_is_over_of_specified()
     {
-        
+        var request = new RequestLoanCreditRequest(
+            2000000.00,
+            "Crédito Direto",
+            80,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(25)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Reprovado");
     }
 
     [Fact]
-    public void Request_loan_credit_is_reproved_when_installment_is_less_of_specified()
+    public async Task Request_loan_credit_is_reproved_when_installment_is_less_of_specified()
     {
-        
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Direto",
+            2,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(25)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Reprovado");
     }
 
     [Fact]
-    public void Request_loan_credit_is_reproved_when_installment_is_over_of_specified()
+    public async Task Request_loan_credit_is_reproved_when_installment_is_over_of_specified()
     {
-        
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Direto",
+            80,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(25)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Reprovado");
     }
 
     [Fact]
-    public void Request_loan_credit_is_reproved_when_value_is_less_of_specified_for_pj()
+    public async Task Request_loan_credit_is_reproved_when_value_is_less_of_specified_for_pj()
     {
-        
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Pessoa Jurídica",
+            5,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(25)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Reprovado");
     }
 
     [Fact]
-    public void Request_loan_credit_is_reproved_when_first_due_date_is_less_of_specified()
+    public async Task Request_loan_credit_is_reproved_when_first_due_date_is_less_of_specified()
     {
-        
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Direto",
+            5,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(2)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Reprovado");
     }
 
     [Fact]
-    public void Request_loan_credit_is_reproved_when_first_due_date_is_over_of_specified()
+    public async Task Request_loan_credit_is_reproved_when_first_due_date_is_over_of_specified()
     {
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Direto",
+            5,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(43)).ToString());
 
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Reprovado");
     }
 
     [Fact]
@@ -46,15 +106,14 @@ public class RequestLoanCreditHandlerTest
     {
         var request = new RequestLoanCreditRequest(
             1000.00,
-            "direto",
+            "Crédito Direto",
             5,
-            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(5)).ToString());
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(15)).ToString());
 
         var sut = new RequestLoanCreditHandler();
 
         var response = await sut.Handle(request, new CancellationToken());
 
-        response.Status.ShouldBe(0);
         response.StatusAsString.ShouldBe("Aprovado");
         response.DebitoTotal.ShouldBe(1020.00);
         response.ValorDoJuros.ShouldBe(2.0);
