6cae6fb8cec3520eefc27ea5d463ffcb2e0a5705
Bruno <bruno.pacheco@tusta.com.br>
2023-04-27 16:27:45 -0300

    test: create tests for each loan type

--- src/Module/CreditHub/CreditHub.UnitTest/Application/UseCases/RequestLoanCredit/RequestLoanCreditHandlerTest.cs
+++ src/Module/CreditHub/CreditHub.UnitTest/Application/UseCases/RequestLoanCredit/RequestLoanCreditHandlerTest.cs
@@ -118,4 +118,76 @@ public class RequestLoanCreditHandlerTest
         response.DebitoTotal.ShouldBe(1020.00);
         response.ValorDoJuros.ShouldBe(2.0);
     }
+
+    [Fact]
+    public async Task Request_loan_credit_is_approved_for_credito_consignado_when_filled_the_specifications()
+    {
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Consignado",
+            5,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(15)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Aprovado");
+        response.DebitoTotal.ShouldBe(1010.00);
+        response.ValorDoJuros.ShouldBe(1.0);
+    }
+
+    [Fact]
+    public async Task Request_loan_credit_is_approved_for_credito_pessoa_juridica_when_filled_the_specifications()
+    {
+        var request = new RequestLoanCreditRequest(
+            20000.00,
+            "Crédito Pessoa Jurídica",
+            5,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(15)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Aprovado");
+        response.DebitoTotal.ShouldBe(21000.00);
+        response.ValorDoJuros.ShouldBe(5.0);
+    }
+
+    [Fact]
+    public async Task Request_loan_credit_is_approved_for_credito_pessoa_fisica_when_filled_the_specifications()
+    {
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Pessoa Física",
+            5,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(15)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Aprovado");
+        response.DebitoTotal.ShouldBe(1030.00);
+        response.ValorDoJuros.ShouldBe(3.0);
+    }
+
+    [Fact]
+    public async Task Request_loan_credit_is_approved_for_credito_imobiliario_when_filled_the_specifications()
+    {
+        var request = new RequestLoanCreditRequest(
+            1000.00,
+            "Crédito Imobiliário",
+            5,
+            DateOnly.FromDateTime(DateTime.UtcNow.AddDays(15)).ToString());
+
+        var sut = new RequestLoanCreditHandler();
+
+        var response = await sut.Handle(request, new CancellationToken());
+
+        response.StatusAsString.ShouldBe("Aprovado");
+        response.DebitoTotal.ShouldBe(1090.00);
+        response.ValorDoJuros.ShouldBe(9.0);
+    }
 }             
